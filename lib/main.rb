c12926d4bd1112e3ff9540804df05bc4aa717e1a
Aleksi1212 <aleksinoro@gmail.com>
2024-06-06 08:16:20 +0300

    simple cli for testing

--- lib/main.rb
+++ lib/main.rb
@@ -3,7 +3,17 @@
 require_relative 'ruby-kv/serializer'
 require_relative 'ruby-kv/disk_store'
 
-db_store = RubyKV::DiskStore.new
+args = ARGV
 
-db_store.put('Anime', 'sex')
-puts db_store.get('Anime')
+if args.empty?
+  puts 'No arguments provided'
+else
+  db_store = RubyKV::DiskStore.new
+
+  if args[0] == 'put'
+    db_store.put(args[1], args[2])
+    puts 'Data added succesfully'
+  elsif args[0] == 'get'
+    puts db_store.get(args[1])
+  end
+end
