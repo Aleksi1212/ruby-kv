8016c8a686f9fe57be4595b973838776cba7d18c
Aleksi1212 <aleksinoro@gmail.com>
2024-06-06 08:17:41 +0300

    initialize disk_store with existing data file

--- lib/ruby-kv/disk_store.rb
+++ lib/ruby-kv/disk_store.rb
@@ -8,6 +8,8 @@ module RubyKV
       @db_fh = File.open(db_file, 'a+b')
       @write_pos = 0
       @key_dir = {}
+
+      init_key_dir
     end
 
     def get(key)
@@ -40,11 +42,34 @@ module RubyKV
     end
 
     def incr_write_pos(pos)
-        @write_pos += pos
+      @write_pos += pos
     end
 
     def key_struct(write_pos, log_size, key)
       { write_pos: write_pos, log_size: log_size, key: key }
+    end
+
+    def init_key_dir
+      while (crc_and_header_bytes = @db_fh.read(crc32_header_offset))
+        header_bytes = crc_and_header_bytes[crc32_offset..]
+        epoch, keysz, valuesz, key_type, value_type = deserialize_header(header_bytes)
+
+        key_bytes = @db_fh.read(keysz)
+        value_bytes = @db_fh.read(valuesz)
+
+        key = unpack(key_bytes, key_type)
+        value = unpack(value_bytes, value_type)
+
+        crc = crc_and_header_bytes[..crc32_offset - 1]
+        raise StandardError, 'file corrupted' unless crc32_valid?(
+          deserialize_crc32(crc),
+          header_bytes + key_bytes + value_bytes
+        )
+
+        log_size = crc32_header_offset + keysz + valuesz
+        @key_dir[key] = key_struct(@write_pos, log_size, key)
+        incr_write_pos(log_size)
+      end
     end
   end
 end
