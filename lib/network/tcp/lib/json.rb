6b9be42fe5bc7822340401a1ba8a05aff09b1b87
Aleksi1212 <aleksinoro@gmail.com>
2024-06-19 14:27:54 +0300

    more schemas

--- lib/network/tcp/lib/json.rb
+++ lib/network/tcp/lib/json.rb
@@ -3,24 +3,36 @@
 require 'json'
 require 'json-schema'
 
-SCHEMA = {
-  'type' => 'object',
-  'required' => %w[method key],
-  'properties' => {
-    'method' => { 'type' => 'string' },
-    'key' => { 'type' => 'string' },
-    'value' => {
-      'anyOf' => [
-        { 'type' => 'string' },
-        { 'type' => 'number' }
-      ]
+module Schemas
+  KV = {
+    'type' => 'object',
+    'required' => %w[method key],
+    'properties' => {
+      'method' => { 'type' => 'string' },
+      'key' => { 'type' => 'string' },
+      'value' => {
+        'anyOf' => [
+          { 'type' => 'string' },
+          { 'type' => 'number' }
+        ]
+      }
     }
-  }
-}.freeze
+  }.freeze
 
-def parse_json(json_string)
+  AUTH = {
+    'type' => 'object',
+    'required' => %w[method user_name password],
+    'properties' => {
+      'method' => { 'const' => 'AUTH' },
+      'user_name' => { 'type' => 'string' },
+      'password' => { 'type' => 'string' }
+    }
+  }.freeze
+end
+
+def parse_json(json_string, schema)
   json_data = JSON.parse(json_string)
-  valid_json = JSON::Validator.validate(SCHEMA, json_data)
+  valid_json = JSON::Validator.validate(schema, json_data)
 
   { error: !valid_json, data: valid_json ? json_data : 'ERR: invalid json data' }
 rescue JSON::ParserError, TypeError => e
